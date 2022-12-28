3f05534a3046e1defa4e293dc1fb5130fc417b3e
DarwishKhan1 <darwishkhan772@gmail.com>
2022-12-28 12:58:26 +0500

    server url changed

--- src/Components/utils/utils.js
+++ src/Components/utils/utils.js
@@ -1,3 +1,3 @@
 // export const localDomain = "https://synectics.pk/api/";
 export const localDomain = "http://localhost:5000/api/";
-export const productionDomain = "https://godh.vercel.app/api/";
+export const productionDomain = "https://goud-server.vercel.app/api/";
